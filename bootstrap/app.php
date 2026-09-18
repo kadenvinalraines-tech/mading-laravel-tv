2a65c0b6f312b5cc3e03d7fe8b0429baeebb4e34
root <root@zhouziyi.zhouziyi>
2026-09-17 20:56:26 -0400

    fix(bootstrap): configure exception handling and file session driver
    Add withExceptions callback in Application builder to resolve ExceptionHandler binding.
    Configure default file session and cache drivers.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -12,4 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Features\Auth\Middleware\RoleMiddleware::class,
         ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
     })->create();
